dc1fc06c0e4c0d147ad2af247eb173df735ffae2
jaspreetsingh <jaspreetsingh1982@gmail.com>
2025-11-08 16:56:00 +1100

    Corrected the output value

--- terraform/modules/storage-account/outputs.tf
+++ terraform/modules/storage-account/outputs.tf
@@ -1,5 +1,5 @@
 output "resourcegroup_name" {
-  value = azurerm_resource_group.resource_group_name.name
+  value = azurerm_resource_group.resource_group_name
 }
 output "storage_account_name" {
   value = azurerm_storage_account.storage.name
