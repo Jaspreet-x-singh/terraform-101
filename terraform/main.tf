9eaabfd7735696ced2a2eeec7fdd1ccfcaa5f600
jaspreetsingh <jaspreetsingh1982@gmail.com>
2025-11-08 15:23:08 +1100

    Updated main and variables.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-rg-resources"
-    storage_account_name = "stterraformbasics01 "
+    storage_account_name = "stterraformbasics01"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
   }
--- terraform/modules/resource-group/variables.tf
+++ terraform/modules/resource-group/variables.tf
@@ -1,6 +1,9 @@
 variable "rgname" {
-  type = string
+  description = "Name of the resource group"
+  type        = string
 }
+
 variable "location" {
-  type = string
+  description = "Azure region for the resource group"
+  type        = string
 }
